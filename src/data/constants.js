d86b588f96e520953b2eaad13a8e07c64411d40f
Zamin Abbas <zaminabbas084@gmail.com>
2025-04-02 19:27:09 +0500

    check

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "AI-Engineer",
   ],
   description:
-    "Hi, I’m Zamin Abbas, a Python Developer with a strong focus on backend development, API integration, and machine learning. I enjoy building scalable applications, optimizing performance, and solving complex problems through clean and efficient code. My expertise includes working with FastAPI, Flask, and Django, along with database management and cloud services",
+    "Hi, I’m Zamin Abbas, a Python Developer with a strong focus on backend development, API integration, and machine learning. I enjoy building scalable applications, optimizing performance, and solving complex problems through clean and efficient code. My expertise includes working with Python, FastAPI, Flask, and Django, along with database management and cloud services",
   github: "https://github.com/Abbas1412",
   resume:
     "https://drive.google.com/file/d/110DuFtP50oEz5vpjcbp3Pz9QtRIrQNXm/view",
@@ -25,7 +25,7 @@ export const skills = [
       {
         name: "Python",
         image:
-          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+          "https://m.media-amazon.com/images/I/41kzzC0zazL._AC_UF1000,1000_QL80_.jpg",
       },
       {
         name: "Bash",
